d8dc985eaf6b991b822c029169cc324d7893d8c9
SimonF4 <simonetti_francoandres@hotmail.com>
2024-01-22 03:12:48 -0300

    Add solution for point 1.

--- src/ar/edu/ort/tp1/ejs01/Main.java
+++ src/ar/edu/ort/tp1/ejs01/Main.java
@@ -3,6 +3,11 @@ package ar.edu.ort.tp1.ejs01;
 public class Main {
 
 	public static void main(String[] args) {
+		// Desde el main():
+
+		// 1- Creá una instancia de Grupo asignándole el número, la letra o el nombre
+		// que le hayan puesto.
+		Grupo miGrupo = new Grupo("Mi Grupo");
 		
 	}
 
