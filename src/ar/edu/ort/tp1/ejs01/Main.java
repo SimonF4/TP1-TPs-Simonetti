713700c34a72d3e8ee2fa0e7c8f01480e9cdc0ef
SimonF4 <simonetti_francoandres@hotmail.com>
2024-01-22 03:18:18 -0300

    Add solution for point 2.

--- src/ar/edu/ort/tp1/ejs01/Main.java
+++ src/ar/edu/ort/tp1/ejs01/Main.java
@@ -9,6 +9,21 @@ public class Main {
 		// que le hayan puesto.
 		Grupo miGrupo = new Grupo("Mi Grupo");
 		
+		
+		// 2- Agregale los miembros reales de tu grupo de trabajos prácticos. Agregale
+		// uno más, ficticio. Cada vez que agregues un integrante el programa debe
+		// mostrar el mensaje “[nombreIntegrante] fue asignado al grupo [nombreGrupo]”,
+		// donde [nombreIntegrante] y [nombreGrupo] son, respectivamente, los nombres
+		// del nuevo integrante y del grupo.
+		miGrupo.agregarIntegrante("integrante1");
+		miGrupo.agregarIntegrante("integrante2");
+		miGrupo.agregarIntegrante("integrante3");
+		miGrupo.agregarIntegrante("integrante4");
+		miGrupo.agregarIntegrante("integranteFicticio");
+		
+		System.out.println("-".repeat(40));
+		
+		
 	}
 
 }
