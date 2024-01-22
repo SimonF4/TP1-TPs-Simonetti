cfcb325fa7bea440b0efd29637aa89ecb100e6ff
SimonF4 <simonetti_francoandres@hotmail.com>
2024-01-22 15:56:59 -0300

    Add enunciado

--- src/ar/edu/ort/tp1/ejs02/Main.java
+++ src/ar/edu/ort/tp1/ejs02/Main.java
@@ -3,6 +3,14 @@ package ar.edu.ort.tp1.ejs02;
 public class Main {
 
 	public static void main(String[] args) {
+		/**
+		 * "Ejercicio 2. Para los Juegos Olímpicos de la Juventud nos pidieron un
+		 * programa con el cual podamos calcular quién fue el ganador de una carrera.
+		 * Para eso ingresaremos un atleta y los segundos que ha empleado (con
+		 * decimales) para recorrer la distancia de una especialidad determinada. Se
+		 * debe tener en cuenta que puede haber más de un ganador (sus tiempos empleados
+		 * fueron los mismos). "
+		 */
 		
 	}
 }
