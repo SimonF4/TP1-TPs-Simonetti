7e1010b182cae19317a76cf0016cc7774695d883
SimonF4 <simonetti_francoandres@hotmail.com>
2024-01-22 22:57:35 -0300

    Add basic attributes and methods

--- src/ar/edu/ort/tp1/ejs02/Carrera.java
+++ src/ar/edu/ort/tp1/ejs02/Carrera.java
@@ -1,5 +1,18 @@
 package ar.edu.ort.tp1.ejs02;
 
+import java.util.ArrayList;
+
 public class Carrera {
 
+	private String nombreEspecialidad;
+	private ArrayList<Atleta> atletas;
+	
+	public Carrera(String nombreEspecialidad) {
+		this.nombreEspecialidad = nombreEspecialidad;
+		this.atletas = new ArrayList<Atleta>();
+	}
+
+	public boolean add(Atleta atleta) {
+		return atletas.add(atleta);
+	}
 }
