ea3378e52bfcb82a4f973f63e0063f87cdd973fa
SimonF4 <simonetti_francoandres@hotmail.com>
2024-01-22 22:29:30 -0300

    Complete class Atleta

--- src/ar/edu/ort/tp1/ejs02/Atleta.java
+++ src/ar/edu/ort/tp1/ejs02/Atleta.java
@@ -1,5 +1,21 @@
 package ar.edu.ort.tp1.ejs02;
 
 public class Atleta {
+	
+	private float tiempoLlegada;
+	private String nombreEspecialidad;
 
+	public Atleta(float tiempoLlegada, String nombreEspecialidad) {
+		this.tiempoLlegada = tiempoLlegada;
+		this.nombreEspecialidad = nombreEspecialidad;
+	}
+	
+	/**
+	 * Para comparar simplemente si es el mismo nombre de especialidad.
+	 * @param nombreEspecialidad
+	 * @return
+	 */
+	public boolean mismaEspecialidad(String nombreEspecialidad) {
+		return this.nombreEspecialidad.equalsIgnoreCase(nombreEspecialidad);
+	}
 }
