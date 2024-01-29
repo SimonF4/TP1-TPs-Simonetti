c16cfa9e02e2b192623d8167e52eae5b51944186
SimonF4 <simonetti_francoandres@hotmail.com>
2024-01-28 23:59:54 -0300

    Complete class - JuegoOlimpico

--- src/ar/edu/ort/tp1/ejs02/JuegoOlimpico.java
+++ src/ar/edu/ort/tp1/ejs02/JuegoOlimpico.java
@@ -1,12 +1,23 @@
 package ar.edu.ort.tp1.ejs02;
 
+import java.util.ArrayList;
+
 public class JuegoOlimpico {
 
-	private Atleta 
+	private Carrera carrera;
 	
-	
-	
-	public void calcularGanador() {
+
+	public void setCarrera(Carrera carrera) throws IllegalArgumentException { 
+		if (carrera != null) {
+			throw new IllegalArgumentException("Error: Se ingreso una carrera nula.");
+		}else if (! carrera.tieneAtletas()) {
+			throw new IllegalArgumentException("Error: Se ingreso una carrera sin atletas.");			
+		}
 		
+		this.carrera = carrera;			
+	}
+
+	public void mostrarGanador() {
+		this.carrera.mostrarGanador();
 	}
 }
